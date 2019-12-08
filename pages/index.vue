323677535b28e9f064b5f5cfc365b7c9c70d6305
nabil <nabil@raudhahpay.com>
2019-12-08 12:01:19 +0800

    no message

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="jumbotron">wedding love love</div>
+  </div>
+</template>
+
+<script>
+    export default {
+        components: {}
+    }
+</script>
+
+<style>
+
+</style>
